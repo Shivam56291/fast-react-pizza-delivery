a7da0a4e24db0eadbbaefcb211828e374e4adfab
Shivam Kumar <roysinghaniashivam@gmail.com>
2025-09-14 15:35:25 +0530

    completed project and add all commit at once

---
+++ src/ui/Button.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Button({ children, disabled, to, type, onClick }) {
+  const base =
+    'inline-block text-sm disabled:cursor-not-allowed" rounded-full bg-yellow-400 font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none ';
+
+  const styles = {
+    primary: base + ' px-4 py-3 md:px-6 md:py-4',
+    small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
+    round: base + ' px-2.5 py-1 md:px-3.5 md:py-2 text-sm',
+    secondary:
+      'inline-block text-sm disabled:cursor-not-allowed" rounded-full font-semibold tracking-wide text-stone-400 uppercase transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:ring focus:ring-stone-300 focus:text-stone-800 focus:ring-offset-2 focus:outline-none border-2 border-stone-300 px-4 py-2.5 md:px-6 md:py-3.5',
+  };
+
+  if (to)
+    return (
+      <Link to={to} className={styles[type]}>
+        {children}
+      </Link>
+    );
+
+  if (onClick)
+    return (
+      <button disabled={disabled} className={styles[type]} onClick={onClick}>
+        {children}
+      </button>
+    );
+
+  return (
+    <button disabled={disabled} className={styles[type]}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
